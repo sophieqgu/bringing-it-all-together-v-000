cccc9211aadf378a82d7d674b7963af761bdf37c
Sophie Gu <sophieqgu@gmail.com>
2019-06-14 16:11:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -73,7 +73,15 @@ class Dog
   
   
   def self.find_by_id(id)
-    
+    sql = <<-SQL
+      SELECT * FROM dogs 
+      WHERE id = ?
+    SQL
+    DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first 
+  end 
+  
   
       
 end 
